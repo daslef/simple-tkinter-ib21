abc8fb7b51de788b5a8a196061c5c9cb3309f677
daslef <daslef93@gmail.com>
2020-03-02 11:58:22 +0300

    frame and binding added

--- 1.py
+++ 1.py
@@ -1,14 +1,39 @@
 from tkinter import *
 
+counter = 0
 
-def add_label():
-    Label(root, text="New Label", padx="30", pady="20", fg="#eeeeee", bg="#333333").pack(side=BOTTOM, fill=X)
+def add_label(event=None):
+    global counter
+    
+    user_text = entry.get()
+    entry.delete(0, END)
+    label_new = Label(root, text=user_text, padx="30", pady="20")
+    
+    if counter % 2:
+        label_new.configure(fg="#eeeeee", bg="#333333")
+    else:
+        label_new.configure(bg="#eeeeee", fg="#333333")
+    
+    label_new.pack(side=BOTTOM, fill=X)
+
+    counter += 1
 
 
 root = Tk()
 root.geometry("300x500")
+root.resizable(False, False)
+root.title("Simple tk")
 
 Label(root, text="Just a test", padx="30", pady="20").pack(side=BOTTOM)
-Button(root, text="Click me!", command=add_label).pack()
+input_frame = Frame(root)
+input_frame.pack()
+
+entry = Entry(input_frame, width="15")
+entry.pack(side=LEFT)
+
+button = Button(input_frame, text="Click me!", command=add_label)
+button.pack(side=LEFT)
+
+entry.bind('<Return>', add_label)
 
 root.mainloop()
